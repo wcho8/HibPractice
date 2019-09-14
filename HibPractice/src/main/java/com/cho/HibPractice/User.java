951ce620d864a8d9de0a88bf2c9773ac58d4ac0b
wcho8 <wcho8@wisc.edu>
2019-09-14 18:05:27 -0500

    initial commit

---
+++ HibPractice/src/main/java/com/cho/HibPractice/User.java
@@ -0,0 +1,48 @@
+package com.cho.HibPractice;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "UserId", unique = true)
+	private int id;
+
+	@Column(name = "FName", nullable = false)
+	private String FName;
+	
+	@Column(name = "LName", nullable = false)
+	private String LName;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFName() {
+		return FName;
+	}
+	
+	public void setFName(String fName) {
+		FName = fName;
+	}
+	
+	public String getLName() {
+		return LName;
+	}
+	
+	public void setLName(String lName) {
+		LName = lName;
+	}
+}
